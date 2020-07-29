845e550961a98dff7d5e65c65c740dbfbca16348
HUEN_OH <heons921@gmail.com>
2020-07-28 20:36:01 -0400

    change it to the original implemetation

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the game</p>
-        {/* <input type="text" name="name" 
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the game</p> */}
+        <input type="text" name="name" 
           value={'Welcome to the game'}
           autoFocus
           class="no-outline"
-        /> */}
+        />
       </header>
     </div>
   );
