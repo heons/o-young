b9bfca46f51f1638fbd7d04f7e115f4adee1e2f1
HUEN_OH <heons921@gmail.com>
2020-07-28 20:23:39 -0400

    test-revert page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>Welcome to the game</p> */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the game</p>
         <input type="text" name="name" 
           value={'Welcome to the game'}
           autoFocus
