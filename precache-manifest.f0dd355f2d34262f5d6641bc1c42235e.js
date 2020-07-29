f1dc8c44d04d0650fdee680146d02d8131aecc0a
HUEN_OH <heons921@gmail.com>
2020-07-28 20:14:59 -0400

    Updates

---
+++ precache-manifest.f0dd355f2d34262f5d6641bc1c42235e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca65242f1f01ce7052f3dd42e8a13624",
+    "url": "/heons/o-young/index.html"
+  },
+  {
+    "revision": "326ead5fe0b14d3a547e",
+    "url": "/heons/o-young/static/css/main.88bff1ce.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/heons/o-young/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heons/o-young/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326ead5fe0b14d3a547e",
+    "url": "/heons/o-young/static/js/main.494f222e.chunk.js"
+  },
+  {
+    "revision": "658e88eef0a1a0bf5e8a",
+    "url": "/heons/o-young/static/js/runtime-main.bfba4847.js"
+  },
+  {
+    "revision": "be3c4a43db1bfe054eb4b58ea6dbece8",
+    "url": "/heons/o-young/static/media/Vogue.be3c4a43.ttf"
+  },
+  {
+    "revision": "5b66fbf1fd05b3698514c9b3cf43aaf5",
+    "url": "/heons/o-young/static/media/Williwaw-Book.5b66fbf1.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/heons/o-young/static/media/logo.5d5d9eef.svg"
+  }
+]);
